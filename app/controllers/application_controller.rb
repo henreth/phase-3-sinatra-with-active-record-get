3796be217b38ab4ca20a3acece56a27e414285cf
Henry Miller <mail.henrym+git@gmail.com>
2022-03-03 21:31:12 -0500

    commit tree e4a3c6ae60778a5366c7cb88c572d608c5bb6b75

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,41 @@
 class ApplicationController < Sinatra::Base
 
+  # Add this line to set the Content-Type header for all responses
+  # tells the client what the type of content on this page is
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+
+    # games = Game.all.order(:title)
+    # games.to_json
+
+    # games = Game.all.order(:title).limit(10)
+    # games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # game.to_json()
+    # include associated reviews in the JSON response
+    # game.to_json(include: :reviews)
+    # include associated users with the reviews
+    game.to_json(include: { reviews: { include: :user } })
+    # only get certain attributes
+    # game.to_json(only: [:id, :title, :genre, :price], include: {
+    #   reviews: { only: [:comment, :score], include: {
+    #     user: { only: [:name] }
+    #   } }
+    # })
   end
 
 end
